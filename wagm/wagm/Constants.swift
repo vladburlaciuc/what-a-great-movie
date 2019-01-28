c792d4790f7d33c4b8c424e0e4b4b8d03d5d558e
wld <wld@wlds-MacBook.local>
2019-01-28 17:56:16 +0200

    add SwiftyJSON pod

---
+++ wagm/wagm/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  wagm
+//
+//  Created by wld on 28/01/2019.
+//  Copyright © 2019 Vlad Burlaciuc. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
---
+++ wagm/wagm/PlainMovie.swift
@@ -0,0 +1,9 @@
+//
+//  PlainMovie.swift
+//  What a great movie!
+//
+//  Created by wld on 28/01/2019.
+//  Copyright © 2019 Vlad Burlaciuc. All rights reserved.
+//
+
+import Foundation
